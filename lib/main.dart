5652e0bc9114b7e2c92a75ee9b629c3fd9868620
mlg2034 <ashatkabytaev@gmail.com>
2024-09-29 11:59:57 +0500

    added firebase

--- lib/main.dart
+++ lib/main.dart
@@ -4,11 +4,18 @@ import 'package:acces_make_mobile/src/features/app/screen/app.dart';
 import 'package:face_camera/face_camera.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+import 'firebase_options.dart';
 
 void main() async {
   configureDependencies();
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   Bloc.observer = GlobalBlocObserver();
-  await FaceCamera.initialize(); 
+  await FaceCamera.initialize();
 
   WidgetsFlutterBinding.ensureInitialized();
   runApp(const AccessMakeApp());
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,11 +5,13 @@
 import FlutterMacOS
 import Foundation
 
+import firebase_core
 import local_auth_darwin
 import path_provider_foundation
 import sqflite
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLALocalAuthPlugin.register(with: registry.registrar(forPlugin: "FLALocalAuthPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
